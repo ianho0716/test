e02fef01f6a5dec0c55cfcc4e0112aa3df60e0dd
Ian Ho <ianho@IandeMacBook-Pro.local>
2022-03-10 06:45:31 +0800

    密碼輸入

---
+++ password1.py
@@ -0,0 +1,14 @@
+password = 'a123456'
+i = 3
+while i > 0:
+	psw = input('請輸入密碼：')
+	if psw == password:
+		print('登入成功！')
+		break
+	else:
+		i = i - 1
+		print('登入錯誤，還有', i, '次機會')
+		if i == 0:
+			print('登入失敗！')
+			break
+			

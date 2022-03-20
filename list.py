5f53c7331c6a90904391f43da8bb9cf04302dd83
Ian Ho <ianho@IandeMacBook-Pro.local>
2022-03-21 00:40:06 +0800

    清單

---
+++ list.py
@@ -0,0 +1,9 @@
+a = ['Benz', 'BMW'] #list清單[index索引]
+print(a)
+print(a[0])
+print(a[1])
+a.append('Bentley') #清單名.append(新增index)
+print(a)
+print(len(a)) #list長度
+print('Bentley' in a)
+print('Toyota' in a)
